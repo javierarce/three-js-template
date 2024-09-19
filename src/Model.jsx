1462e57a16341c440cb430f4b21257c026cd9331
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-09-19 19:52:54 +0200

    Adds photo

--- src/Model.jsx
+++ src/Model.jsx
@@ -3,11 +3,15 @@ import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
+import { useTexture, useAspect } from "@react-three/drei";
+
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 
 export default function Model() {
   const meshRef = useRef();
+  const texture = useTexture("./src/assets/photo.jpg");
+  const scale = useAspect(texture.image.width, texture.image.height, 0.5);
 
   useFrame(({ clock }) => {
     if (meshRef.current) {
@@ -15,18 +19,24 @@ export default function Model() {
     }
   });
 
+  const uniforms = useRef({
+    uTime: { value: 0 },
+    uTexture: { value: texture },
+    uBackColor: { value: new THREE.Color(0.1, 0.3, 0.5) },
+    uResolution: {
+      value: new THREE.Vector2(texture.image.width, texture.image.height),
+    },
+  });
+
   return (
-    <mesh>
-      <planeGeometry args={[2, 2, 10, 10]} />
+    <mesh scale={scale}>
+      <planeGeometry args={[1, 1, 10, 10]} />
       <shaderMaterial
         ref={meshRef}
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
         side={THREE.DoubleSide}
-        uniforms={{
-          uTime: { value: 0 },
-          uBackColor: { value: new THREE.Color(0.1, 0.3, 0.5) },
-        }}
+        uniforms={uniforms.current}
       />
     </mesh>
   );
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,12 @@
-varying vec3 vUv;
+varying vec2 vUv;
 uniform float uTime;
 uniform vec3 uBackColor;
+uniform sampler2D uTexture;
 
 void main() {
     if (gl_FrontFacing) {
-        gl_FragColor = vec4(sin(uTime * 2.0), vUv.x, 1.0, 1.0);
+        // gl_FragColor = vec4(sin(uTime * 2.0), vUv.x, 1.0, 1.0);
+        gl_FragColor = texture2D(uTexture, vUv);
     } else {
         gl_FragColor = vec4(uBackColor, 1.0);
     }
